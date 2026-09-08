7861f506cb14f32346f87d50af88f9fb85e324d1
B-SMART <B-SMART@DESKTOP-G9ILLTJ>
2026-09-08 11:35:43 +0300

    Fix subscription screen asset cache busting

--- public/js/feature-loader.js
+++ public/js/feature-loader.js
@@ -82,7 +82,7 @@
         'saas-billing': {
             dependencies: [],
             styles: [],
-            scripts: ['/js/pages/saas/saas.js?v=1']
+            scripts: ['/js/pages/saas/saas.js?v=2']
         },
         'backup-history': {
             dependencies: [],
